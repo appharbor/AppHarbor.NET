a27c89d3d7f068a4c8b59c355fda9ae00d75aecb
Christian Bay <cb@appharbor.com>
2012-05-24 13:48:55 +0200

    Refactor: Removed unused usings

--- AppHarbor.Net/Model/Collaborator.cs
+++ AppHarbor.Net/Model/Collaborator.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace AppHarbor.Model
+﻿namespace AppHarbor.Model
 {
     public class Collaborator : IUrl, IKeyed
     {
--- Samples/AppHarbor.Sample/Models/AccountModels.cs
+++ Samples/AppHarbor.Sample/Models/AccountModels.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Globalization;
-using System.Web.Mvc;
-using System.Web.Security;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace AppHarbor.Sample.Models
 {
