81a50181ed77662aeaede5afb06d5bdf15b958b4
Christian Bay <cb@appharbor.com>
2012-05-24 14:37:15 +0200

    Refactor: Extracted AppHarborApi's error part into its own file

---
+++ AppHarbor.Net/AppHarborApi.Error.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using AppHarbor.Model;
+using RestSharp;
+
+namespace AppHarbor
+{
+	public partial class AppHarborApi
+	{
+		public Error GetError(string applicationID, long ID)
+		{
+			CheckArgumentNull("applicationID", applicationID);
+
+			var request = new RestRequest();
+			request.Resource = "applications/{applicationID}/errors/{ID}";
+			request.AddParameter("applicationID", applicationID, ParameterType.UrlSegment);
+			request.AddParameter("ID", ID, ParameterType.UrlSegment);
+
+			return ExecuteGetKeyed<Error>(request);
+		}
+
+		public IList<Error> GetErrors(string applicationID)
+		{
+			CheckArgumentNull("applicationID", applicationID);
+
+			var request = new RestRequest();
+			request.Resource = "applications/{applicationID}/errors";
+			request.AddParameter("applicationID", applicationID, ParameterType.UrlSegment);
+
+			return ExecuteGetListKeyed<Error>(request);
+		}
+	}
+}
--- AppHarbor.Net/AppHarborApi.cs
+++ AppHarbor.Net/AppHarborApi.cs
@@ -646,33 +646,6 @@ namespace AppHarbor
 
 		#endregion
 
-		#region Error
-
-		public Error GetError(string applicationID, long ID)
-		{
-			CheckArgumentNull("applicationID", applicationID);
-
-			var request = new RestRequest();
-			request.Resource = "applications/{applicationID}/errors/{ID}";
-			request.AddParameter("applicationID", applicationID, ParameterType.UrlSegment);
-			request.AddParameter("ID", ID, ParameterType.UrlSegment);
-
-			return ExecuteGetKeyed<Error>(request);
-		}
-
-		public IList<Error> GetErrors(string applicationID)
-		{
-			CheckArgumentNull("applicationID", applicationID);
-
-			var request = new RestRequest();
-			request.Resource = "applications/{applicationID}/errors";
-			request.AddParameter("applicationID", applicationID, ParameterType.UrlSegment);
-
-			return ExecuteGetListKeyed<Error>(request);
-		}
-
-		#endregion
-
 		#region User
 
 		public User GetUser()
