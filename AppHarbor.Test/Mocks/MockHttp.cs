ffab06483e1fe64c64dcadfcba9358f3b0827db4
Christian Bay <cb@appharbor.com>
2012-06-04 11:03:23 +0200

    Refactor: Removed brackets from case statements without local variables

--- AppHarbor.Test/Mocks/MockHttp.cs
+++ AppHarbor.Test/Mocks/MockHttp.cs
@@ -18,13 +18,9 @@ namespace AppHarbor.Test.Mocks
 				case "/applications/:application/configurationvariables/5":
 				case "/applications/:application/hostnames/5":
 				case "/applications/:application/servicehooks/5":
-				{
 					return CreateHttpResponse(HttpStatusCode.NoContent, null);
-				}
 				default:
-				{
 					return CreateHttpResponse(HttpStatusCode.NotFound, null);
-				}
 			}
 		}
 
@@ -34,77 +30,41 @@ namespace AppHarbor.Test.Mocks
 			switch (url)
 			{
 				case "/applications":
-				{
 					return CreateGetResponse(Url, HttpStatusCode.OK, Path.Combine(BaseLocation, @"Data\GetApplications.json"));
-				}
 				case "/applications/:application":
-				{
 					return CreateGetResponse(Url, HttpStatusCode.OK, Path.Combine(BaseLocation, @"Data\GetApplication.json"));
-				}
 				case "/applications/:application/collaborators":
-				{
 					return CreateGetResponse(Url, HttpStatusCode.OK, Path.Combine(BaseLocation, @"Data\GetCollaborators.json"));
-				}
 				case "/applications/:application/collaborators/5":
-				{
 					return CreateGetResponse(Url, HttpStatusCode.OK, Path.Combine(BaseLocation, @"Data\GetCollaborator.json"));
-				}
 				case "/applications/:application/configurationvariables":
-				{
 					return CreateGetResponse(Url, HttpStatusCode.OK, Path.Combine(BaseLocation, @"Data\GetConfigurationVariables.json"));
-				}
 				case "/applications/:application/configurationvariables/5":
-				{
 					return CreateGetResponse(Url, HttpStatusCode.OK, Path.Combine(BaseLocation, @"Data\GetConfigurationVariable.json"));
-				}
 				case "/applications/:application/hostnames":
-				{
 					return CreateGetResponse(Url, HttpStatusCode.OK, Path.Combine(BaseLocation, @"Data\GetHostnames.json"));
-				}
 				case "/applications/:application/hostnames/5":
-				{
 					return CreateGetResponse(Url, HttpStatusCode.OK, Path.Combine(BaseLocation, @"Data\GetHostname.json"));
-				}
 				case "/applications/:application/servicehooks":
-				{
 					return CreateGetResponse(Url, HttpStatusCode.OK, Path.Combine(BaseLocation, @"Data\GetServiceHooks.json"));
-				}
 				case "/applications/:application/servicehooks/5":
-				{
 					return CreateGetResponse(Url, HttpStatusCode.OK, Path.Combine(BaseLocation, @"Data\GetServiceHook.json"));
-				}
 				case "/applications/:application/builds":
-				{
 					return CreateGetResponse(Url, HttpStatusCode.OK, Path.Combine(BaseLocation, @"Data\GetBuilds.json"));
-				}
 				case "/applications/:application/builds/5":
-				{
 					return CreateGetResponse(Url, HttpStatusCode.OK, Path.Combine(BaseLocation, @"Data\GetBuild.json"));
-				}
 				case "/applications/:application/builds/5/tests":
-				{
 					return CreateGetResponse(Url, HttpStatusCode.OK, Path.Combine(BaseLocation, @"Data\GetTests.json"));
-				}
 				case "/applications/:application/builds/5/tests/3.1":
-				{
 					return CreateGetResponse(Url, HttpStatusCode.OK, Path.Combine(BaseLocation, @"Data\GetTest.json"));
-				}
 				case "/applications/:application/errors":
-				{
 					return CreateGetResponse(Url, HttpStatusCode.OK, Path.Combine(BaseLocation, @"Data\GetErrors.json"));
-				}
 				case "/applications/:application/errors/5":
-				{
 					return CreateGetResponse(Url, HttpStatusCode.OK, Path.Combine(BaseLocation, @"Data\GetError.json"));
-				}
 				case "/user":
-				{
 					return CreateGetResponse(Url, HttpStatusCode.OK, Path.Combine(BaseLocation, @"Data\GetUser.json"));
-				}
 				default:
-				{
 					return CreateHttpResponse(HttpStatusCode.NotFound, null);
-				}
 			}
 		}
 
@@ -114,29 +74,17 @@ namespace AppHarbor.Test.Mocks
 			switch (url)
 			{
 				case "/applications":
-				{
 					return CreateHttpResponse(HttpStatusCode.Created, "https://appharbor.com/applications/:application");
-				}
 				case "/applications/:application/collaborators":
-				{
 					return CreateHttpResponse(HttpStatusCode.Created, "https://appharbor.com/applications/:application/collaborators/5");
-				}
 				case "/applications/:application/configurationvariables":
-				{
 					return CreateHttpResponse(HttpStatusCode.Created, "https://appharbor.com/applications/:application/configurationvariables/5");
-				}
 				case "/applications/:application/hostnames":
-				{
 					return CreateHttpResponse(HttpStatusCode.Created, "https://appharbor.com/applications/:application/hostnames/5");
-				}
 				case "/applications/:application/servicehooks":
-				{
 					return CreateHttpResponse(HttpStatusCode.Created, "https://appharbor.com/applications/:application/servicehooks/5");
-				}
 				default:
-				{
 					return CreateHttpResponse(HttpStatusCode.BadRequest, null);
-				}
 			}
 		}
 
@@ -148,13 +96,9 @@ namespace AppHarbor.Test.Mocks
 				case "/applications/:application":
 				case "/applications/:application/collaborators/5":
 				case "/applications/:application/configurationvariables/5":
-				{
 					return CreateHttpResponse(HttpStatusCode.OK, null);
-				}
 				default:
-				{
 					return CreateHttpResponse(HttpStatusCode.NotFound, null);
-				}
 			}
 		}
 	}
@@ -174,13 +118,9 @@ namespace AppHarbor.Test.Mocks
 				case "/applications/:application/builds":
 				case "/applications/:application/builds/5/tests":
 				case "/applications/:application/errors":
-				{
 					return CreateGetResponse(Url, HttpStatusCode.OK, Path.Combine(BaseLocation, @"Data\EmptyArray.json"));
-				}
 				default:
-				{
 					return CreateHttpResponse(HttpStatusCode.NotFound, null);
-				}
 			}
 		}
 	}
@@ -197,13 +137,9 @@ namespace AppHarbor.Test.Mocks
 				case "/applications/:application/configurationvariables":
 				case "/applications/:application/hostnames":
 				case "/applications/:application/servicehooks":
-				{
 					return CreateHttpResponse(HttpStatusCode.Conflict, null);
-				}
 				default:
-				{
 					return CreateHttpResponse(HttpStatusCode.BadRequest, null);
-				}
 			}
 		}
 	}
