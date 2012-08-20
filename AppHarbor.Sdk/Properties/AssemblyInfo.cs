46b5a6184660d3c9a5edd6c96916205c78fb1e4e
Christian Bay <cb@appharbor.com>
2012-08-20 15:02:22 +0200

    Increased version to 2.2.0.0

--- AppHarbor.Sdk/Properties/AssemblyInfo.cs
+++ AppHarbor.Sdk/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("60a17348-1fe6-45e4-8769-1ea923322d51")]
 
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
